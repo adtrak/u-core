04fcb93654ee72b2c0a0610bb7f1414c93c89e59
Alex Dudley <alex.dudley@adtrak.co.uk>
2024-12-19 14:08:40 +0000

    Trying to get carousel working

--- assets/carousels.js
+++ assets/carousels.js
@@ -1,35 +1,21 @@
-// Trending products carousel
+// Product page image Swiper
 document.addEventListener('DOMContentLoaded', function() {
     try {
-        const swiper = new Swiper('.swiper-trending-products', {
-        direction: 'horizontal',
+        const swiper = new Swiper('.swiper', {
+        // direction: 'horizontal',
+        slidesPerView: 1,
+        // loop: true,
         pagination: {
-            el: '.swiper-pagination-featured-products',
-            clickable: true
+            el: '.swiper-pagination',
+            clickable: true,
         },
-        navigation: {
-            prevEl: '.swiper-button-prev',
-            nextEl: '.swiper-button-next'
-        },
-        autoplay: false,
-        breakpoints: {
-            320: {
-            slidesPerView: 1,
-            spaceBetween: 20
-            },
-            767: {
-            slidesPerView: 1,
-            spaceBetween: 30
-            },
-            1200: {
-            slidesPerView: 1,
-            spaceBetween: 40
-            },
-            1600: {
-            slidesPerView: 4.2,
-            spaceBetween: 60
-            }
-        }
+        // navigation: {
+        //     nextEl: '.swiper-button-next',
+        //     prevEl: '.swiper-button-prev',
+        // },
+        // autoplay: {
+        //     delay: 5000,
+        //   },
         })
     } catch (e) {
         console.log("SwiperJS is not defined");
