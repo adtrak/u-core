8ed4caa682fbddf7e0d513b2e1668d9411c079d2
Alex Dudley <alex.dudley@adtrak.co.uk>
2024-12-20 15:49:40 +0000

    Progress made fixing the slider

--- assets/carousels.js
+++ assets/carousels.js
@@ -1,21 +1,14 @@
 // Product page image Swiper
 document.addEventListener('DOMContentLoaded', function() {
     try {
-        const swiper = new Swiper('.swiper', {
+        const swiper = new Swiper('.swiper-featured-products', {
         // direction: 'horizontal',
-        slidesPerView: 1,
+        slidesPerView: 2,
         // loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,
-        },
-        // navigation: {
-        //     nextEl: '.swiper-button-next',
-        //     prevEl: '.swiper-button-prev',
-        // },
-        // autoplay: {
-        //     delay: 5000,
-        //   },
+        scrollbar: {
+            el: ".swiper-featured-products-scrollbar",
+            hide: true,
+          },
         })
     } catch (e) {
         console.log("SwiperJS is not defined");
